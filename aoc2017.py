3944a4295eb61e86e57d6f77e844a5bec9246c87
Lukas Raffelt <lukas.raffelt@googlemail.com>
2021-12-02 21:41:56 +0100

    initial setup

---
+++ aoc2017.py
@@ -0,0 +1,12 @@
+from day01 import day01
+from helper import pretty
+
+
+
+def runAll():
+    pretty.greeting()
+    day01.part1()
+
+if __name__ == '__main__':
+    pretty.greeting()
+    runAll()
---
+++ template/test_day__.py
@@ -0,0 +1,22 @@
+import day__ as day
+
+INPUTFOLDER = day.get_path()
+
+def test_part_1():
+    result = day.run_part_1(INPUTFOLDER+"/test")
+    assert result == 0
+
+
+def test_part_1_real():
+    result = day.run_part_1(INPUTFOLDER+"/input")
+    assert result == 0
+
+
+def test_part_2():
+    result = day.run_part_2(INPUTFOLDER+"/test")
+    assert result == 0
+
+
+def test_part_2_real():
+    result = day.run_part_2(INPUTFOLDER+"/input")
+    assert result == 0
