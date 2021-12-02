405a4848e9be61d69bc367909037d1a32c2124dc
Lukas Raffelt <lukas.raffelt@googlemail.com>
2021-12-02 21:59:59 +0100

    bla

--- day01/test_day01.py
+++ day01/test_day01.py
@@ -3,20 +3,20 @@ import day01 as day
 INPUTFOLDER = day.get_path()
 
 def test_part_1():
-    result = day.run_part_1(INPUTFOLDER+"/test1")
+    result = day.run_part_1(INPUTFOLDER+"/test")
     assert result == 0
 
 
 def test_part_1_real():
-    result = day.run_part_1(INPUTFOLDER+"/input1")
+    result = day.run_part_1(INPUTFOLDER+"/input")
     assert result == 0
 
 
 def test_part_2():
-    result = day.run_part_2(INPUTFOLDER+"/test2")
+    result = day.run_part_2(INPUTFOLDER+"/test")
     assert result == 0
 
 
 def test_part_2_real():
-    result = day.run_part_2(INPUTFOLDER+"/input2")
+    result = day.run_part_2(INPUTFOLDER+"/input")
     assert result == 0
